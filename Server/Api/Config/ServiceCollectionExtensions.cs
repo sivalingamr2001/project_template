2ec5357985979c14b610d0b1cde372408e10d9d4
Sivalingam R <sivalinga1902@gmail.com>
2026-04-21 16:58:02 +0530

    change the api

--- Server/Api/Config/ServiceCollectionExtensions.cs
+++ Server/Api/Config/ServiceCollectionExtensions.cs
@@ -74,7 +74,7 @@ public static class ServiceCollectionExtensions
         {
             options.AddPolicy(CorsPolicyNames.ReactClient, policy =>
             {
-                policy.WithOrigins("http://localhost:5173")
+                policy.WithOrigins("http://localhost:5174", "http://localhost:5173")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
